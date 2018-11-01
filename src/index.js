c4921e59ae03071bc46d0c9f4543a3d4b7a94165
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2018-11-01 23:54:38 +0200

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-require('babel-register')({
-    presets: ['power-assert'],
-    ignore: /node_modules/
+require('@babel/register')({
+    presets: ['power-assert', ['@babel/preset-env', {targets: {node: 'current'}}]],
+    ignore: [/node_modules/]
 });
 global.assert = require('power-assert');
