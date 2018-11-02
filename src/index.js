eec1399bb1e6add1ee6a277b959e021f81e67279
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2018-11-02 17:38:52 +0200

    use @babel/plugin-proposal-export-{default,namespace}-from

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 require('@babel/register')({
+    plugins: [
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-proposal-export-namespace-from'
+    ],
     presets: ['power-assert', ['@babel/preset-env', {targets: {node: 'current'}}]],
     ignore: [/node_modules/]
 });
